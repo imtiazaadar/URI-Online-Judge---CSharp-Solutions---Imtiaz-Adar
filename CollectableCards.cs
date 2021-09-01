169be86cca436848624d4f92259c43c1add7d17f
Imtiaz Adar <75721079+imtiazaadar@users.noreply.github.com>
2021-09-01 12:30:11 +0600

    Add files via upload

---
+++ CollectableCards.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImtiazAdar
+{
+    class CollectableCards
+    {
+        //static int gcd(int a, int b)
+        //{
+        //    if(b == 0)
+        //        return a;
+        //    return gcd(b, a % b);
+        //}
+        //static void Main(string[] args)
+        //{
+        //    int N = int.Parse(Console.ReadLine());
+        //    while(N-- > 0)
+        //    {
+        //        string[] sen = Console.ReadLine().Split(' ');
+        //        int x = int.Parse(sen[0]);
+        //        int y = int.Parse(sen[1]);
+        //        int ans = gcd(x, y);
+        //        Console.WriteLine(ans);
+        //    }
+        //}
+    }
+}
---
+++ GetlineThree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImtiazAdar
+{
+    class GetlineThree
+    {
+        static void Main(string[] args)
+        {
+            string sen;
+            int casee = 0;
+            while((sen = Console.ReadLine()) != null)
+            {
+                casee++;
+                if (casee > 1)
+                    Console.WriteLine();
+                string[] newsen = Console.ReadLine().Split(' ');
+                int male = 0, female = 0, total = 0;
+                for(int i = 0; i < newsen.Length - 1; i++)
+                {
+                    if(newsen[i] == sen)
+                    {
+                        total++;
+                        if (newsen[i + 1] == "M")
+                            male++;
+                        else if (newsen[i + 1] == "F")
+                            female++;
+                    }
+                }
+                Console.WriteLine($"Caso {casee}:");
+                Console.WriteLine($"Pares Iguais: {total}");
+                Console.WriteLine($"F: {female}");
+                Console.WriteLine($"M: {male}");
+            }
+        }
+    }
+}
